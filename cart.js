ec48430eb9985086b24e9ad8183b57817321a48f
Віктор <agor832@gmail.com>
2025-07-20 10:45:29 +0300

    Delete cart.js

--- cart.js
+++
@@ -1,8 +0,0 @@
-const cart = JSON.parse(localStorage.getItem('cart') || '[]');
-fetch('products.json')
-  .then(res => res.json())
-  .then(products => {
-    const container = document.getElementById('cart-items');
-    const selected = products.filter(p => cart.includes(p.id));
-    container.innerHTML = selected.map(p => `<p>${p.name} – ${p.price} ₴</p>`).join('');
-  });
